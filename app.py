fbd45af7809be68cfe078ecdd9533cbf5556c507
David Kirwan <david.kirwan@gmail.com>
2024-05-13 17:41:22 +0200

    Porfolio creation

--- app.py
+++ app.py
@@ -14,9 +14,15 @@ class Asset(db.Model):
     def __repr__(self):
         return f"Asset('{self.name}', {self.value})"
 
-with app.app_context():
-    db.create_all()
-
+@app.route('/create_portfolio', methods=['POST'])
+def create_portfolio():
+    name = request.form['name']
+    if name:
+        new_portfolio = Portfolio(name=name)
+        db.session.add(new_portfolio)
+        db.session.commit()
+        return redirect(url_for('main.hello_world'))
+    return 'Missing data', 400
 
 class Portfolio(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -24,6 +30,11 @@ class Portfolio(db.Model):
 
     def __repr__(self):
         return f"Portfolio('{self.name}')"
+
+
+with app.app_context():
+    db.create_all()
+
 
 main = Blueprint('main', __name__)
 
@@ -36,17 +47,17 @@ def hello_world():
     links = []
     for rule in app.url_map.iter_rules():
         try:
-            url = url_for(rule.endpoint, _external=True, **{arg: 'dummy' for arg in rule.arguments})
+            placeholders = {arg: 1 if converter.__class__.__name__ == 'IntegerConverter' else 'dummy' for arg, converter in rule._converters.items()}
+            url = url_for(rule.endpoint, _external=True, **placeholders)
         except BuildError:
             url = None
         links.append((url, rule.endpoint))
     return render_template('index.html', current_time=current_time, links=links)
 
-
-@main.route('/portfolios', methods=['GET'])
-def get_portfolios():
-    portfolios = Portfolio.query.all()
-    return jsonify([{'id': p.id, 'name': p.name} for p in portfolios])
+# @main.route('/portfolios', methods=['GET'])
+# def get_portfolios():
+#     portfolios = Portfolio.query.all()
+#     return jsonify([{'id': p.id, 'name': p.name} for p in portfolios])
 
 app.register_blueprint(main)
 
@@ -76,10 +87,49 @@ def add_asset():
     return 'Missing data', 400
 
 
-@app.route('/portfolios')
+# @app.route('/portfolios')
+# def portfolios():
+#     portfolios = Portfolio.query.all()
+#     return render_template('portfolios.html', portfolios=portfolios)
+
+@app.route('/portfolios', methods=['GET', 'POST'])
 def portfolios():
-    portfolios = Portfolio.query.all()
-    return render_template('portfolios.html', portfolios=portfolios)
+    if request.method == 'POST':
+        name = request.form['name']
+        print(f"Received POST request with name: {name}")
+        if name:
+            new_portfolio = Portfolio(name=name)
+            db.session.add(new_portfolio)
+            db.session.commit()
+        return redirect(url_for('portfolios'))
+    else:
+        portfolios = Portfolio.query.all()
+        print("Portfolios:", portfolios)
+        return render_template('portfolios.html', portfolios=portfolios)
+
+@app.route('/modify_portfolio/<id>', methods=['GET', 'POST'])
+def modify_portfolio(id):
+    print(f"modify_portfolio called with id={id}")
+    try:
+        id = int(id)
+    except ValueError:
+        return "Invalid portfolio id", 400
+
+    portfolio = Portfolio.query.get_or_404(id)
+    if request.method == 'POST':
+        name = request.form['name']
+        if name:
+            portfolio.name = name
+            db.session.commit()
+            return redirect(url_for('portfolios'))
+    return render_template('modify_portfolio.html', portfolio=portfolio)
+
+@app.route('/delete_portfolio/<int:id>', methods=['POST'])
+def delete_portfolio(id):
+    portfolio = Portfolio.query.get_or_404(id)
+    db.session.delete(portfolio)
+    db.session.commit()
+    return redirect(url_for('portfolios'))
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True)
